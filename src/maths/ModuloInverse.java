082ff4abf91d4e424389c6a9f887d1c0aab5d56c
Koushik Dandapat <koushikdandapat16@gmail.com>
2022-12-24 22:52:36 +0530

    upload

--- src/maths/ModuloInverse.java
+++ src/maths/ModuloInverse.java
@@ -1,9 +1,37 @@
 package maths;
 
 public class ModuloInverse {
+	
+
+   public static  int modInverse(int A, int M) {
+     int g = gcd(A, M);
+     if (g != 1)
+        return -1;
+     else {
+       
+         return  power(A, M - 2, M);
+     }
+ }
+
+   public static int power(int x, int y, int M)  {
+     if (y == 0)
+         return 1;
+     int p = power(x, y / 2, M) % M;
+     p = (int)((p * (long)p) % M);
+     if (y % 2 == 0)
+         return p;
+     else
+         return (int)((x * (long)p) % M);
+ }
+
+   public static int gcd(int a, int b) {
+     if (a == 0)
+         return b;
+     return gcd(b % a, a);
+ }
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
 
 	}
 
